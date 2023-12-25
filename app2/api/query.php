1bb7fb3a976e1e1d23819671a8b9db0d9bb8b675
rudy870103 <favoriteinfinite@gmail.com>
2023-12-25 16:12:48 +0800

    修正按鈕顯示問題

--- app2/api/query.php
+++ app2/api/query.php
@@ -24,14 +24,14 @@ switch($_GET['do']){
             }
         }
         $in=join(',',$nums);
-        $users=$Student->q("select `name`,`uni_id`,`school_num`,`birthday` from `students` where `id` in($in)");
+        $users=$Student->q("select `id`,`name`,`uni_id`,`school_num`,`birthday` from `students` where `id` in($in)");
 
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($users);
             
     break;
     case 'classes':
-        $classes=$Class->q("select `id`,`code`,`name` from `classes`");
+        $classes=$Class->q("select `code`,`name` from `classes`");
         header('Content-Type: application/json; charset=utf-8');        
         echo json_encode($classes);
     break;
